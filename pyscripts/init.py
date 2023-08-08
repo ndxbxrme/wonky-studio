d4e3503f354c67b14366e05dc5e89b0b0776f437
ndxbxrme <lewis_the_cat@hotmail.com>
2023-08-08 13:12:27 +0100

    init

---
+++ pyscripts/init.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+os.mkdir('./rendered')
+os.mkdir('./rendered/audio')
+os.mkdir('./rendered/audio/en')
+os.mkdir('./rendered/data')
+os.mkdir('./rendered/data/en')
+os.mkdir('./sources')
+os.mkdir('./sources/audio')
+os.mkdir('./sources/audio/en')
+os.mkdir('./sources/data')
+os.mkdir('./sources/data/en')
+os.mkdir('./sources/visual')
+os.mkdir('./sources/visual/en')
+
+sources = []
+game = {
+    "rooms": []
+}
+script = []
+with open('./rendered/data/en/game.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(game))
+with open('./sources/data/en/script.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(script))
+with open('./rendered/data/en/script-processed-wav.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(script))
+with open('./sources/sources.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(sources))
